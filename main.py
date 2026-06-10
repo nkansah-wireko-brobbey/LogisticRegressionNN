b304c48f8aac0d4ed79c078ba346a0e1242fca73
Brobbey <nkansahwirekobrobbey@gmail.com>
2026-06-09 23:57:34 -0500

    wip: initialization logic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import numpy as np
+
 
 def main():
     print("Logistic Regression With Neural Network")
 
+def logistic_regression(sample_data: list, y: list):
+    X = np.array(sample_data)
+    y = np.array(y)
+    n_features = X.shape[1]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
