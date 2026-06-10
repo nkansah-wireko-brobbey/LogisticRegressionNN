d3538d51b462891513ddc5c97819abbbbf473d78
Brobbey <nkansahwirekobrobbey@gmail.com>
2026-06-10 00:02:02 -0500

    feat: implement base initialization logic

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def logistic_regression(sample_data: list, y: list):
     X = np.array(sample_data)
     y = np.array(y)
     n_features = X.shape[1]
+    W = np.random.randn(n_features, 1)
+    b = 0
+
+    learning_rate = 0.01
+    epochs = 1000
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
